55cfdbd083d2041d8a818e0d08e57c47e481317c
luisdralves <luisalves@untile.pt>
2023-05-05 01:07:51 +0100

    Update font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import './index.css';
-import '@fontsource/raleway/300-italic.css';
-import '@fontsource/raleway/300.css';
+import '@fontsource/josefin-sans/300-italic.css';
+import '@fontsource/josefin-sans/300.css';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import App from './App.tsx';
 import React from 'react';
