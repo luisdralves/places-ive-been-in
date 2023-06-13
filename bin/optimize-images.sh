f8bd6035f2cabc64eac6679c8dad4ee2d7221480
luisdralves <luisalves@untile.pt>
2023-06-13 23:24:49 +0000

    Enforce bash in shell script and add starter script

--- bin/optimize-images.sh
+++ bin/optimize-images.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd ./public/images
-find . -type d -exec sh -c '[ ! -d "../thumbnails/${0:2}" ] && echo "creating ${0}" && mkdir "../thumbnails/${0:2}"' {} \;
-find . -type f \( ! -iname ".*" \) -exec sh -c '[ ! -f "../thumbnails/${0:2}" ] && echo "converting ${0}" && convert -resize 600 "$0" "../thumbnails/${0:2}"' {} \;
+find . -type d -exec bash -c '[ ! -d "../thumbnails/${0:2}" ] && echo "creating ${0}" && mkdir "../thumbnails/${0:2}"' {} \;
+find . -type f \( ! -iname ".*" \) -exec bash -c '[ ! -f "../thumbnails/${0:2}" ] && echo "converting ${0}" && convert -resize 600 "$0" "../thumbnails/${0:2}"' {} \;
 
 cd ../thumbnails
-find . -type d -exec sh -c '[ ! -d "../images/${0:2}" ] && echo "removing ${0}" && rm -r "${0}"' {} \;
-find . -type f \( ! -iname ".*" \) -exec sh -c '[ ! -f "../images/${0:2}" ] && echo "removing ${0}" && rm "${0}"' {} \;
+find . -type d -exec bash -c '[ ! -d "../images/${0:2}" ] && echo "removing ${0}" && rm -r "${0}"' {} \;
+find . -type f \( ! -iname ".*" \) -exec bash -c '[ ! -f "../images/${0:2}" ] && echo "removing ${0}" && rm "${0}"' {} \;
---
+++ up.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+cd /mnt/bigdisk/home/miguel/Documents/projects/react/places-ive-been-in
+yarn build
+yarn preview --host
