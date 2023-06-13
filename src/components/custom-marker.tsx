6c9a71f785c53a5826ac9145c62cefbe8dde4008
luisdralves <luisalves@untile.pt>
2023-06-14 00:15:38 +0100

    Add timeline component

--- src/components/custom-marker.tsx
+++ src/components/custom-marker.tsx
@@ -2,26 +2,37 @@ import { Marker } from 'react-map-gl';
 import { ReactComponent as MarkerIcon } from 'src/assets/svg/marker.svg';
 import { Point } from 'src/core/config/points';
 import { colors } from 'src/core/config/colors';
+import { getColor } from 'src/core/utils/dates';
+import { useMemo } from 'react';
 
 type Props = {
   onClick?: () => void;
   point: Point;
 };
 
-export const CustomMarker = ({ onClick, point }: Props) => (
-  <Marker
-    anchor={'bottom'}
-    latitude={point.lat}
-    longitude={point.lon}
-    offset={[0, 8]}
-    onClick={onClick}
-    style={{zIndex:colors.length - point.color}}
-  >
-    <MarkerIcon
-      color={colors[point.color]}
+export const CustomMarker = ({ onClick, point }: Props) => {
+  const autoColor = useMemo(
+    () => colors.findIndex(color => color === getColor(point.dates)),
+    [point.dates]
+  );
+
+  const colorIndex = point.color ?? autoColor;
+
+  return (
+    <Marker
+      anchor={'bottom'}
+      latitude={point.lat}
+      longitude={point.lon}
+      offset={[0, 8]}
       onClick={onClick}
-      style={{ cursor: 'pointer' }}
-      width={32}
-    />
-  </Marker>
-);
+      style={{ zIndex: colors.length - colorIndex }}
+    >
+      <MarkerIcon
+        color={colors[colorIndex]}
+        onClick={onClick}
+        style={{ cursor: 'pointer' }}
+        width={32}
+      />
+    </Marker>
+  );
+};
---
+++ src/core/utils/dates.ts
@@ -0,0 +1,33 @@
+import { colors } from 'src/core/config/colors';
+
+export const getTotalTime = (dates: [string, string?][] = []) => {
+  return dates.reduce((total, [start, end]) => {
+    return (
+      total + (new Date(end ?? start).getTime() + 24 * 60 * 60 * 1000 - new Date(start).getTime())
+    );
+  }, 0);
+};
+
+export const getColor = (dates: [string, string?][] = []) => {
+  const totalTime = getTotalTime(dates);
+
+  if (totalTime > 30 * 24 * 60 * 60 * 1000) {
+    return colors[0];
+  }
+
+  if (
+    (totalTime > 14 * 24 * 60 * 60 * 1000 && dates.length === 1) ||
+    (totalTime > 7 * 24 * 60 * 60 * 1000 && dates.length > 1)
+  ) {
+    return colors[1];
+  }
+
+  if (totalTime > 24 * 60 * 60 * 1000) {
+    return colors[2];
+  }
+
+  return colors[3];
+};
+
+export const formatMonth = (date: Date) =>
+  `${date.toDateString().split(' ')[1]} ${date.getFullYear()}`;
