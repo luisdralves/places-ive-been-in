a175b8afd8d69131dc8d717f0202db54ef42a76c
luisdralves <luisalves@untile.pt>
2025-11-04 21:47:21 +0000

    Add support for HEIC

--- bin/optimize-images.sh
+++ bin/optimize-images.sh
@@ -2,8 +2,8 @@
 
 cd ./public/images
 find . -type d -exec bash -c '[ ! -d "../thumbnails/${0:2}" ] && echo "creating ${0}" && mkdir "../thumbnails/${0:2}"' {} \;
-find . -type f \( ! -iname ".*" \) -exec bash -c '[ ! -f "../thumbnails/${0:2}" ] && echo "converting ${0}" && convert -resize 600 "$0" "../thumbnails/${0:2}"' {} \;
+find . -type f \( ! -iname ".*" \) -exec bash -c 'base="${0:2}"; [ ! -f "../thumbnails/${base}" ] && echo "converting ${0}" && magick "$0" -resize 600 "../thumbnails/${base%.*}.webp"' {} \;
 
 cd ../thumbnails
 find . -type d -exec bash -c '[ ! -d "../images/${0:2}" ] && echo "removing ${0}" && rm -r "${0}"' {} \;
-find . -type f \( ! -iname ".*" \) -exec bash -c '[ ! -f "../images/${0:2}" ] && echo "removing ${0}" && rm "${0}"' {} \;
+find . -type f \( ! -iname ".*" \) -exec bash -c 'base="${0:2}"; basename="${base%.*}"; [ ! -f "../images/${basename}."* ] && echo "removing ${0}" && rm "${0}"' {} \;
